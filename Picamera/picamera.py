72eb9246e33edf061762a0914c397ceb84068cbe
Hoturan <davidwcorral@hotmail.com>
2017-07-13 12:49:31 +0200

    Picamera cambiado
    Error implica parar el programa

--- Picamera/picamera.py
+++ Picamera/picamera.py
@@ -1,7 +1,6 @@
 import picamera
 import RPi.GPIO as GPIO
-
-error = False
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -12,26 +11,14 @@ camera.start_recording('1.h264')
 try:
     camera.wait_recording(30)
 except PiCameraError:
-    error = True
-
-if (not error):
     GPIO.output(18,GPIO.HIGH)
-    time.sleep(0.10) #tiempo con LED encendido si no hay error
-    GPIO.output(18,GPIO.LOW)
-else:
-    GPIO.output(18,GPIO.HIGH)
+    sys.exit()
 
 for i in range(2, 100):
     camera.split_recording('%d.h264' % i)
     try:
     	camera.wait_recording(30)
     except PiCameraError:
-    	error = True
-
-    if (not error):
         GPIO.output(18,GPIO.HIGH)
-        time.sleep(0.10)
-        GPIO.output(18,GPIO.LOW)
-    else:
-        GPIO.output(18,GPIO.HIGH)
+        sys.exit()
     camera.stop_recording()
