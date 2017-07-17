7820b9a0a666185177a0273c49402e7d4e1e86b7
J0oan <jmitjansblasco@gmail.com>
2017-07-17 00:02:32 +0000

    Now, when the rpi is rebooted the video files are not overwrited

--- src/rpi_cam_gps/Processes/CamPoller.py
+++ src/rpi_cam_gps/Processes/CamPoller.py
@@ -1,6 +1,7 @@
 import picamera
 import RPi.GPIO as GPIO
 import threading
+import os.path
 
 camera = None
 
@@ -17,8 +18,11 @@ class CamPoller(threading.Thread):
     
   def run(self):
 	global camera
-	i = 1
-	camera.start_recording('/home/pi/Videos/1.h264')
+	for i in range (1,100):
+	  if not os.path.exists('/home/pi/Videos/%d.h264' % i):
+		break
+	  i = i + 1
+	camera.start_recording('/home/pi/Videos/%d.h264' % i)
 	try:
 	  camera.wait_recording(5)
 	except PiCameraError:
