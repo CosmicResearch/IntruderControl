3cab5718d3d2a792ab20ef8b59b6d1dae3fa93e5
jmitjansblasco@gmail.com <jmitjansblasco@gmail.com>
2017-07-19 02:36:50 +0200

    Code compatible with rpi zero. Reference gpio changed to GPIO.BOARD for better caompatibility between raspberrys. The video files are stored with the same format name vid_0001 in order to sort them properly. Now you can choose the fps, resolution, where storage videos ... all from the config file (les hardcoded)

--- src/rpi_cam_gps/Processes/CamPoller.py
+++ src/rpi_cam_gps/Processes/CamPoller.py
@@ -5,6 +5,9 @@ import os.path
 import ConfigParser
 
 camera = None
+led = 12
+duration = 30
+path = '/home/pi/Videos/'
 
 class CamPoller(threading.Thread):
   def __init__(self):
@@ -12,32 +15,37 @@ class CamPoller(threading.Thread):
     config = ConfigParser.ConfigParser()
     config.read("/home/pi/IntruderControl/src/rpi_cam_gps/config.ini")
     led = int(config.get("LED", "ledCam"))
-    GPIO.setmode(GPIO.BCM)
+    fps = int(config.get("CAMERA","fps"))
+    resx = int(config.get("CAMERA","resolutionX"))
+    resy = int(config.get("CAMERA","resolutionY"))
+    duration = int(config.get("CAMERA","duration"))
+    path = str(config.get("CAMERA","savedPath"))
+    GPIO.setmode(GPIO.BOARD)
     GPIO.setwarnings(False)
     GPIO.setup(led,GPIO.OUT)
     GPIO.output(led,GPIO.HIGH)
     global camera
-    camera=picamera.PiCamera(resolution=(1640, 1232),framerate=30)
+    camera=picamera.PiCamera(resolution=(resx, resy),framerate=fps)
     self.current_value = None
     self.running = True
     
   def run(self):
 	global camera
 	for i in range (1,100):
-	  if not os.path.exists('/home/pi/Videos/%d.h264' % i):
-		break
-	  i = i + 1
-	GPIO.output(18,GPIO.LOW)
-	camera.start_recording('/home/pi/Videos/%d.h264' % i)
+	 	if not os.path.exists(path+'vid_%04d.h264' % i):
+			break
+		i = i + 1
+	GPIO.output(led,GPIO.LOW)
+	camera.start_recording(path+'vid_%04d.h264' % i)
 	try:
-	  camera.wait_recording(30)
+		camera.wait_recording(10)
 	except PiCameraError:
-	  GPIO.output(18,GPIO.HIGH)
+		GPIO.output(led,PIO.HIGH)
 	while self.running:
-	  i=i+1
-	  camera.split_recording('/home/pi/Videos/%d.h264' % i)
-	  try:
-		camera.wait_recording(30)
-	  except PiCameraError:
-		GPIO.output(18,GPIO.HIGH)
+		i=i+1
+		camera.split_recording(path+'vid_%04d.h264' % i)
+		try:
+			camera.wait_recording(duration)
+		except PiCameraError:
+			GPIO.output(led,GPIO.HIGH)
 	camera.stop_recording()
